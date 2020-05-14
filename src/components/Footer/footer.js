77a99f4e7405c4e4fb557b9ce7e475f2ff96b4b0
Your NameZachGaskin <zgaskin@ufclab.com>
2020-05-14 14:27:09 -0400

    netlify case sensitive

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import logo from "../../images/white-jzglogo1.svg"
-import SocialFoot from "./socialFoot"
+import SocialFoot from "./socialfoot"
 
 const Footer = () => {
   return (
